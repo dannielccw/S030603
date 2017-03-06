4235757ffbab27b0276f5119187578f239761ffb
dannielccw <dannielccw@gmail.com>
2017-03-06 15:51:55 +0800

    + another and another: spinner

--- app/src/main/java/com/example/yvtc/s030603/MainActivity.java
+++ app/src/main/java/com/example/yvtc/s030603/MainActivity.java
@@ -5,13 +5,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     Spinner spinner, spinner2;
-    String[] strs = {"AA", "BBB", "CCCC", "DD", "EEEEE"};
+    ArrayList<String> data;
     TextView tv, tv2;
+    EditText ed;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +25,12 @@ public class MainActivity extends AppCompatActivity {
         tv2 = (TextView) findViewById(R.id.textView2);
         spinner = (Spinner) findViewById(R.id.spinner);
         spinner2 = (Spinner) findViewById(R.id.spinner2);
+
+        ed=(EditText) findViewById(R.id.editText);
+        data = new ArrayList<>();
+        data.add("AAA");
+        data.add("BB");
+        data.add("CCCC");
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -35,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        ArrayAdapter<String> adapter =new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, strs);
+        ArrayAdapter<String> adapter =new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, data);
         spinner2.setAdapter(adapter);
     }
 
@@ -45,6 +55,11 @@ public class MainActivity extends AppCompatActivity {
         String[] f= getResources().getStringArray(R.array.fruits);
         tv2.setText(f[poi]);
 
+    }
+
+    public void clickAdd(View v)
+    {
+        data.add(ed.getText().toString());
     }
 
 }
